b1e14b4da7e8bc064d066ec0876f7bd235dc7e21
VALERDOS <valerijfyodorov@gmail.com>
2021-11-13 20:52:58 +0400

    6

--- public_html/application/controllers/MainController.php
+++ public_html/application/controllers/MainController.php
@@ -3,11 +3,18 @@
 namespace application\controllers;
 
 use application\core\Controller;
+use application\lib\Db;
 
 class MainController extends Controller {
 
     public function indexAction(){
+        $db = new Db;
 
+        $form = 2;
+
+        $data =  $db->column('SELECT name FROM users WHERE id ='.$form);
+
+        debug($data);
         $this->view->render('Главная страница');
 
     }
