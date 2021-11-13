5845d9b7d8ecf5bc641626952b8b72e88c013745
VALERDOS <valerijfyodorov@gmail.com>
2021-11-13 15:59:54 +0400

    2

---
+++ public_html/application/controllers/MainController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace application\controllers;
+
+use application\core\Controller;
+
+class MainControllerController extends Controller {
+
+    public function indexAction(){
+        echo 'Главная страница';
+
+    }
+}
